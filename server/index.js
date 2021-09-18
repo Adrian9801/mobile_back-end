1a1f66235d2795b2a77086c248abeb5885d342b0
Adrián Mora <Adri98.1mm@gmail.com>
2021-09-18 10:42:57 -0600

    d

--- server/index.js
+++ server/index.js
@@ -5,16 +5,37 @@ var  app = express();
 var mysql = require('mysql');
 
 
+var db_config = {
+  host: "mysql5046.site4now.net",
+  user: "a79446_mobile",
+  password: "admin123",
+  database: "db_a79446_mobile"
+};
 
-function connectMYSQL(){
-  var con = mysql.createConnection({
-    host: "mysql5046.site4now.net",
-    user: "a79446_mobile",
-    password: "admin123",
-    database: "db_a79446_mobile"
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
   });
-  return con;
 }
+
+handleDisconnect();
 
 app.use(bodyParser.urlencoded({ extended:  true }));
 app.use(bodyParser.json());
@@ -22,19 +43,8 @@ app.use(cors()); // {origin :'http://localhost:8090'}
 
 
 app.get('/Products', function(req, res) {
-  var con = connectMYSQL();
-  con.connect(function(err) {
-    if (err)
-      isConnect = false;
-    else
-      isConnect = true;
-  });
-  if(!isConnect){
-    res.send([[{result: 'failConect'}]]);
-    return;
-  }
   var sql = 'SELECT * FROM productos';
-  con.query(sql, function (err, rows) {
+  connection.query(sql, function (err, rows) {
     if (err) 
       res.send([[{result: 'error'}]]);
     else{
@@ -46,17 +56,6 @@ app.get('/Products', function(req, res) {
 });
 
 app.post('/UpadateProduct', function(req, res) {
-  var con = connectMYSQL();
-  con.connect(function(err) {
-    if (err)
-      isConnect = false;
-    else
-      isConnect = true;
-  });
-  if(!isConnect){
-    res.send([[{result: 'failConect'}]]);
-    return;
-  }
   var request = req.body;
   var sql = "UPDATE productos SET codigo = " + mysql.escape(request.codigo) + ", nombre = " + mysql.escape(request.nombre) + ", cantidad = " + mysql.escape(request.cantidad) + " WHERE codigo = " +  mysql.escape(request.codigoAnt);
   con.query(sql, function (err, rows) {
@@ -68,17 +67,6 @@ app.post('/UpadateProduct', function(req, res) {
 });
 
 app.get('/deleteProduct/:codigo', function(req, res) {
-  var con = connectMYSQL();
-  con.connect(function(err) {
-    if (err)
-      isConnect = false;
-    else
-      isConnect = true;
-  });
-  if(!isConnect){
-    res.send([[{result: 'failConect'}]]);
-    return;
-  }
   var sql = 'DELETE FROM productos WHERE codigo = ' + mysql.escape(req.params.codigo);
   con.query(sql, function (err, result) {
     if (err)
@@ -89,18 +77,6 @@ app.get('/deleteProduct/:codigo', function(req, res) {
 });
 
 app.post('/InsertProduct', function(req, res) {
-  var con = connectMYSQL();
-  con.connect(function(err) {
-    if (err)
-      isConnect = false;
-    else
-      isConnect = true;
-  });
-  if(!isConnect){
-    res.send([[{result: 'failConect'}]]);
-    return;
-  }
-
   var request = req.body;
   var sql = "INSERT INTO productos (codigo, nombre, cantidad) VALUES (" + mysql.escape(request.codigo) + ", " + mysql.escape(request.nombre) + ", " + mysql.escape(request.cantidad) + ")";
   con.query(sql, function (err, result) {
